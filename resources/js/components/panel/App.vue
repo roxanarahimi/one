8e0b9324c2918b90bec1d1707af2af3fa676e636
Roxana <ms.roxanarahimi@gmail.com>
2022-07-14 18:49:57 +0430

    style

--- resources/js/components/panel/App.vue
+++ resources/js/components/panel/App.vue
@@ -10,9 +10,9 @@
     <div id="page" class="d-flex bg-dark"
          v-if="this.$route.name !== 'Login0' && this.$route.fullPath !== '/panel/login' && this.$route.fullPath !== '/panel/register' && this.$route.fullPath !== '/panel/reset/password' && this.$route.name !== 'Error404'">
 
-        <div id="sidebar-wrapper " class=" bg-dark" >
+        <div id="sidebar-wrapper" class=" bg-dark">
             <div id="sidebar" class="bg-dark text-light">
-                <!--            <h4 class=" my-3 text-center">پنل مدیریت</h4>-->
+                <!--            <h4 class=" my-3 text-center ">پنل مدیریت</h4>-->
                 <ul class="nav nav-pills flex-column mb-auto " style="padding-top: 1px">
                     <li class="mt-2 pt-2">
                         <span class="theme nav-link text-white ">
@@ -95,13 +95,13 @@
                             <span class="sidebar_title">اسلاید ها</span>
                         </router-link>
                     </li>
-<!--                    <li>-->
-<!--                        <router-link to="/panel/icon" class="nav-link text-white"-->
-<!--                                     :class="{active: $route.fullPath ==='/panel/icon'}">-->
-<!--                            <i class="bi bi-heptagon-fill me-2"></i>-->
-<!--                            <span class="sidebar_title">لوگو/ آیکون</span>-->
-<!--                        </router-link>-->
-<!--                    </li>-->
+                    <!--                    <li>-->
+                    <!--                        <router-link to="/panel/icon" class="nav-link text-white"-->
+                    <!--                                     :class="{active: $route.fullPath ==='/panel/icon'}">-->
+                    <!--                            <i class="bi bi-heptagon-fill me-2"></i>-->
+                    <!--                            <span class="sidebar_title">لوگو/ آیکون</span>-->
+                    <!--                        </router-link>-->
+                    <!--                    </li>-->
 
                     <li>
                         <router-link to="/panel/teachers" class="nav-link text-white"
@@ -159,9 +159,9 @@
             </div>
         </div>
         <main id="main" class="wrapper  bg-light">
-            <nav class="navbar navbar-expand  navbar-dark bg-dark text-light " >
+            <nav class="navbar navbar-expand  navbar-dark bg-dark text-light">
                 <div class="container-fluid">
-                    <span id="sidebar_toggle_btn" class="fw-bold py-1 pe-2 ps-4" @click="sideBarToggle"
+                    <span id="sidebar_toggle_btn" class="fw-bold py-1 pe-2 mt-2" @click="sideBarToggle"
                           style="font-size: 20px; line-height: 10px; cursor: pointer">
                         <i class="bi bi-list " style="font-size:25px"></i>
                     </span>
